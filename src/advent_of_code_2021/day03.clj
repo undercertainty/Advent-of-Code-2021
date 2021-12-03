ef364880ef37c50378601b6c3e49496e3d6fda64
undercertainty <undercertainty@gmail.com>
2021-12-03 12:25:35 +0000

    Part A done

--- src/advent_of_code_2021/day03.clj
+++ src/advent_of_code_2021/day03.clj
@@ -7,39 +7,103 @@
 
 ;;; Start with a function to parse the input:
 
+; Get as a seq of colls of 1s and 0s
+
 (defn parse-input [file-in]
   (->> file-in
        (slurp)
-       ; Do something here))
+       (re-seq #"[10]+")
+       (map vec)))
 
 (parse-input test-file)
 
-;;; Main task for part a
+; It'll be useful to have a function that orders the
+; members of a coll by how often they appear:
 
-(defn day03a [input]
-    "Function body")
+(defn counter [coll]
+  (->> coll
+       frequencies
+       vec
+       (sort-by second)
+       reverse))
+
+(counter [1 2 2 1 1 1 2 2 1 1 1 1 1 2])
+; ([1 9] [2 5])
+
+; Now want to count the 0s and 1s in each column and find
+; the most common bits
+
+(defn get-gamma [input]
+  (->> input
+       (apply map vector)
+       (map counter)
+       (map first)  ; most common bit
+       (map first)
+       str/join
+       (#(Integer/parseInt % 2))))
 
 (->> test-file
-    (parse-input)
-    day03a)
+     parse-input
+     get-gamma)
+
+(defn get-epsilon [input]
+  (->> input
+       (apply map vector)
+       (map counter)
+       (map second) ; least common bit
+       (map first)
+       str/join
+       (#(Integer/parseInt % 2))))
+
+(->> test-file
+     parse-input
+     get-epsilon)
+
+
+(defn day03a [input]
+  (*
+   (get-gamma input)
+   (get-epsilon input)))
+
+(->> test-file
+     (parse-input)
+     day03a)
 
 (->> puzzle-input
-    (parse-input)
-    day03a)
+     (parse-input)
+     day03a)
+; 3242606
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; Main task for part b
 
+
+
+
+
 (defn day03b [input]
-    "Function body")
+  "Function body")
 
 (->> test-file
-    (parse-input)
-    day03b)
+     (parse-input)
+     day03b)
 
 (->> puzzle-input
-    (parse-input)
-    day03b)
+     (parse-input)
+     day03b)
+
+(defn counter [coll]
+  (->> coll
+       frequencies
+       vec
+       (sort-by second)
+       reverse))
 
 
+(counter [1 2 2 1 1 2  22 2 2 3 2 2])
+
+
+(let [d {:a 1 :b 2 :c 3}]
+  (vec d))
