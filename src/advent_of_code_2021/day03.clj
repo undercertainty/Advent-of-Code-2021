697663c0723d1f350f5d8dfcb220acf90a75ee8b
undercertainty <undercertainty@gmail.com>
2021-12-03 15:25:36 +0000

    Day 03 complete

--- src/advent_of_code_2021/day03.clj
+++ src/advent_of_code_2021/day03.clj
@@ -30,12 +30,15 @@
 (counter [1 2 2 1 1 1 2 2 1 1 1 1 1 2])
 ; ([1 9] [2 5])
 
+(counter (first (parse-input test-file)))
+; ([\0 4] [\1 1])
+
 ; Now want to count the 0s and 1s in each column and find
 ; the most common bits
 
 (defn get-gamma [input]
   (->> input
-       (apply map vector)
+       (apply map vector) ; transpose
        (map counter)
        (map first)  ; most common bit
        (map first)
@@ -50,7 +53,7 @@
   (->> input
        (apply map vector)
        (map counter)
-       (map second) ; least common bit
+       (map last) ; least common bit
        (map first)
        str/join
        (#(Integer/parseInt % 2))))
@@ -79,12 +82,85 @@
 ;;
 ;; Main task for part b
 
+; Want a function to remove the rows with only the
+; most common value in the nth column
 
+; Because of the fiddliness around ties, need a tweak
+; to the counter function, so that if the number of \0
+; and \1 are equal, then it's sorted s.t \1 comes first
+; sort is stable, so we can simply sort and reverse:
 
+(defn counter-b [coll]
+  (->> coll
+       frequencies
+       vec
+       (sort-by first)
+       (sort-by second)
+       reverse))
 
+; Remove all rows whose nth value is not the column's most
+; common value
+(defn filter-by-most-common [input n]
+  (let [most-common (->> input
+                         (apply map vector) ; transpose
+                         (map counter-b)
+                         (#(nth % n))
+                         first
+                         first)]
+    (filter #(=
+              (nth % n)
+              most-common)
+            input)))
 
+; Do the same for least common. Obviously I could munge these
+; into the same function, but I'm not planning to spend longer
+; on this than necessary
+
+(defn filter-by-least-common [input n]
+  (let [least-common (->> input
+                          (apply map vector) ; transpose
+                          (map counter-b)
+                          (#(nth % n))
+                          last
+                          first)]
+    (filter #(=
+              (nth % n)
+              least-common)
+            input)))
+
+(defn get-ogr [input]
+  (->> input
+       (#(reduce
+          filter-by-most-common
+          %
+          (range (count (first input)))))
+       first
+       str/join
+       (#(Integer/parseInt % 2))))
+
+(->> test-file
+     parse-input
+     get-ogr)
+
+(defn get-c02 [input]
+  (->> input
+       (#(reduce
+          filter-by-least-common
+          %
+          (range (count (first input)))))
+       first
+       str/join
+       (#(Integer/parseInt % 2))))
+
+(->> test-file
+     parse-input
+     get-c02)
+
+     
 (defn day03b [input]
-  "Function body")
+  (*
+   (get-ogr input)
+   (get-c02 input)))
 
 (->> test-file
      (parse-input)
@@ -93,17 +169,4 @@
 (->> puzzle-input
      (parse-input)
      day03b)
-
-(defn counter [coll]
-  (->> coll
-       frequencies
-       vec
-       (sort-by second)
-       reverse))
-
-
-(counter [1 2 2 1 1 2  22 2 2 3 2 2])
-
-
-(let [d {:a 1 :b 2 :c 3}]
-  (vec d))
+; 4856080
