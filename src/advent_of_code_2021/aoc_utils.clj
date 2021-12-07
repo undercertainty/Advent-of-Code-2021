cccd0fc715daac5b414e153765efc928c42b164c
undercertainty <undercertainty@gmail.com>
2021-12-07 16:05:45 +0000

    Day 05 complete

--- src/advent_of_code_2021/aoc_utils.clj
+++ src/advent_of_code_2021/aoc_utils.clj
@@ -12,3 +12,19 @@
 (defn transpose [grid]
   (apply map vector grid))
 
+; a range function to count in either direction, including the 
+; end boundaries. Will only run on integers, and increments of 
+; +1 or -1.
+
+(defn inc-range
+  "Range from start to end inclusive in either direction. Single increments only."
+  ([end]
+   (assert (integer? end) "end argument to inc-range must be integer")
+   (inc-range 0 end))
+  ([start end]
+  (assert (integer? start) "start argument to inc-range must be integer")
+  (assert (integer? end) "end argument to inc-range must be integer")
+  (cond
+    (<= start end) (range start (inc end))
+    (> start end) (range start (dec end) -1))))
+
--- src/advent_of_code_2021/day05.clj
+++ src/advent_of_code_2021/day05.clj
@@ -1,6 +1,8 @@
 
 (ns advent-of-code-2021.day05
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:require [clojure.walk :as w])
+  (:require [advent-of-code-2021.aoc-utils :as aoc]))
 
 (def test-file "data/day05/test.txt")
 (def puzzle-input "data/day05/puzzle.txt")
@@ -10,23 +12,67 @@
 (defn parse-input [file-in]
   (->> file-in
        slurp
-       ; Do something here))
+       (re-seq #"(\d+),(\d+) -> (\d+),(\d+)")
+       (map (fn [[_ fromx fromy tox toy]] ; hacky but clear and quick
+              {:fromx (Integer/parseInt fromx)
+               :fromy (Integer/parseInt fromy)
+               :tox (Integer/parseInt tox)
+               :toy (Integer/parseInt toy)}))))
 
-  ))
+(->>
+ (parse-input test-file)
+ (w/walk vals flatten)
+ (apply max))
 
+; I'll use a dense matrix; could be a bit inefficient, but whatever
+
+; Let's just create a matrix of zeros of the longest value provided
+; Needs to be a vector rather than a sequence to easily index
+(defn initial-state [input]
+  (let [max-size (->> input
+                      (w/walk vals flatten)
+                      (apply max))]
+  (into [] 
+        (repeat (inc max-size)
+                (into []
+                      (repeat (inc max-size) 0)))))) ; inc s.t have 0-max range
 
 (-> test-file
-    slurp)
+    parse-input
+    initial-state)
 
+; OK, now thinking numpy/pandas would be nice...
 
+(defn increment-point [matrix {:keys [x y]}]
+  (assoc matrix x
+         (update (matrix x) y inc)))
 
+; Just considering horizontal and vertical lines for
+; now, so in other cases, return empty list
+(defn line-to-points [{:keys [fromx fromy tox toy]}]
+  (cond
+    (= fromx tox) (for [y (aoc/inc-range fromy toy)]
+                    {:x fromx :y y})
+    (= fromy toy) (for [x (aoc/inc-range fromx tox)]
+                    {:x x :y fromy})
+    :else ()))
 
-(parse-input test-file)
+(-> test-file
+    parse-input
+    first
+    line-to-points)
 
 ;;; Main task for part a
 
 (defn day05a [input]
-    "Function body")
+  (->> (reduce
+        increment-point
+        (initial-state input)
+        (flatten (map line-to-points input)))
+
+       flatten
+       (filter #(< 1 %))
+       count))
 
 (->> test-file
     (parse-input)
@@ -35,20 +81,44 @@
 (->> puzzle-input
     (parse-input)
     day05a)
+;7674
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; Main task for part b
 
+; Extend the line-to-points function so that it covers
+; the diagonals too. Not bothering to check that the
+; diagonals work; that's implied by the question.
+
+(defn line-to-points-b [{:keys [fromx fromy tox toy]}]
+  (cond
+    (= fromx tox) (for [y (aoc/inc-range fromy toy)]
+                    {:x fromx :y y})
+    (= fromy toy) (for [x (aoc/inc-range fromx tox)]
+                    {:x x :y fromy})
+    :else (map 
+           (fn [x y] {:x x :y y})
+           (aoc/inc-range fromx tox)
+           (aoc/inc-range fromy toy))))
+
+
 (defn day05b [input]
-    "Function body")
+  (->> (reduce
+        increment-point
+        (initial-state input)
+        (flatten (map line-to-points-b input)))
+
+       flatten
+       (filter #(< 1 %))
+       count))
 
 (->> test-file
-    (parse-input)
-    day05b)
+     (parse-input)
+     day05b)
 
 (->> puzzle-input
-    (parse-input)
-    day05b)
-
+     (parse-input)
+     day05b)
+;20898
 
